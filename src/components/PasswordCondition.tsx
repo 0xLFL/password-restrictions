0c6a36b6a2779353626432aec1249d6c569ebd06
luke <z5363030@unsw.edu.au>
2025-01-28 16:39:01 +1100

    max component more customisable and debugs component

--- src/components/PasswordCondition.tsx
+++ src/components/PasswordCondition.tsx
@@ -3,14 +3,37 @@ import { VscCheck, VscChromeClose } from "react-icons/vsc";
 
 export interface PasswordConditionProps {
   condition: string,
-  passed: boolean
+  passed: boolean,
+  passColor?: string,
+  passIcon?: React.ReactNode,
+  failColor?: string,
+  failIcon?: React.ReactNode,
+  restrictionStyle?: React.CSSProperties,
+  iconStyle?: React.CSSProperties,
 }
 
-const PasswordCondition = ({ condition, passed }: PasswordConditionProps) => {
+const PasswordCondition = ({
+  condition,
+  passed,
+  passColor = '#8BC34A',
+  passIcon,
+  failColor = '#F44336',
+  failIcon,
+  restrictionStyle = {},
+  iconStyle = {},
+}: PasswordConditionProps) => {
+  const resolvedPassSymbol = passIcon || <VscCheck style={iconStyle} fill={passColor} />;
+  const resolvedFailSymbol = failIcon || <VscChromeClose style={iconStyle} fill={failColor} />;
+
   return (
-    <div className='rp-password-condition-container'>
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      gap: '10px',
+      ...restrictionStyle
+    }}>
       {
-        passed ? <VscCheck fill='#8BC34A' /> : <VscChromeClose fill='#F44336' />
+        passed ? resolvedPassSymbol : resolvedFailSymbol
       }
       <p>{ condition }</p>
     </div>
--- src/components/PasswordRestrictions.tsx
+++ src/components/PasswordRestrictions.tsx
@@ -1,16 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PasswordCondition from "./PasswordCondition";
 
 export interface PasswordRestriction {
   condition: string,
-  test: RegExp
+  test: RegExp,
 }
+
+export const defaultRestrictionsIds = {
+  minChar: 'minChar',
+  containLower: 'containLower',
+  containUpper: 'containUpper',
+  containNum: 'containNum',
+  containSpecial: 'containSpecial',
+} as const;
+
+type DefaultRestrictionsIds = typeof defaultRestrictionsIds[keyof typeof defaultRestrictionsIds];
+export type DefaultRestrictions = Record<DefaultRestrictionsIds, boolean | undefined>;
 
 export interface PasswordRestrictionsProps {
   label: string,
   password: string,
-  confirmPassword: string,
-  passwordRestrictions: PasswordRestriction[]
+  confirmPassword?: string,
+  passwordRestrictions?: PasswordRestriction[],
+  configureDefault?: DefaultRestrictions,
+  confirmPasswordCondition?: string,
+  labelStyle?: React.CSSProperties,
+  containerStyle?: React.CSSProperties,
+  restrictionStyle?: React.CSSProperties,
+  iconStyle?: React.CSSProperties,
+  passColor?: string,
+  passIcon?: React.ReactNode,
+  failColor?: string,
+  failIcon?: React.ReactNode,
+}
+
+const defaultRestrictions: Record<DefaultRestrictionsIds, PasswordRestriction> = {
+  [defaultRestrictionsIds.minChar]: {
+    condition: 'Be at least 8 characters long',
+    test: /.{8,}/,
+  },
+  [defaultRestrictionsIds.containLower]: {
+    condition: 'Contain a lowercase letter',
+    test: /[a-z]/,
+  },
+  [defaultRestrictionsIds.containUpper]: {
+    condition: 'Contain a uppercase letter',
+    test: /[A-Z]/,
+  },
+  [defaultRestrictionsIds.containNum]: {
+    condition: 'Contain a number letter',
+    test: /[0-9]/,
+  },
+  [defaultRestrictionsIds.containSpecial]: {
+    condition: 'Contain a special character',
+    test: /[^\w\s]/,
+  }
 }
 
 const PasswordRestrictions = ({
@@ -18,18 +62,67 @@ const PasswordRestrictions = ({
   password,
   confirmPassword,
   passwordRestrictions,
+  configureDefault,
+  confirmPasswordCondition,
+  labelStyle = {},
+  containerStyle = {},
+  restrictionStyle,
+  iconStyle,
+  passColor,
+  passIcon,
+  failColor,
+  failIcon,
 }: PasswordRestrictionsProps) => {
+  const [passwordRestrictions_, setPasswordRestrictions] = useState<PasswordRestriction[]>([]);
+
+  // prevents the passwordRestrictictions from being set every time there is a state change
+  useEffect(() => {
+    const _passwordRestrictions: PasswordRestriction[] = [];
+    if (passwordRestrictions === undefined && configureDefault) {
+      for (const id of Object.values(defaultRestrictionsIds)) {
+        if (configureDefault[id]) {
+          _passwordRestrictions.push(defaultRestrictions[id])
+        }
+      }
+    } else if (passwordRestrictions === undefined) {
+      _passwordRestrictions.push(...Object.values(defaultRestrictions));
+    }
+
+    setPasswordRestrictions(_passwordRestrictions)
+  }, [passwordRestrictions, configureDefault,])
+
   return (
-    <div>
-      <h3>{label}</h3>
+    <div style={containerStyle}>
+      <h3 style={labelStyle}>{label}</h3>
       {
-        passwordRestrictions.map(({ condition, test }, index) => {
+        passwordRestrictions_.map(({ condition, test }, index) => {
           return (
-            <PasswordCondition key={index} condition={condition} passed={test.test(password)}/>
+            <PasswordCondition
+              key={index}
+              condition={condition}
+              passed={test.test(password)}
+              restrictionStyle={restrictionStyle}
+              iconStyle={iconStyle}
+              passColor={passColor}
+              passIcon={passIcon}
+              failColor={failColor}
+              failIcon={failIcon}
+            />
           )
         })
       }
-      <PasswordCondition condition='Match' passed={password === confirmPassword}/>
+      { confirmPassword !== undefined &&
+        <PasswordCondition
+          condition={confirmPasswordCondition || 'Match'}
+          passed={password === confirmPassword}
+          restrictionStyle={restrictionStyle}
+          iconStyle={iconStyle}
+          passColor={passColor}
+          passIcon={passIcon}
+          failColor={failColor}
+          failIcon={failIcon}
+        />
+      }
     </div>
   )
 }
